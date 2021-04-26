851ce0cbd914de6c12c4328b8e8c5ca67cc21052
drjimoh <waliu.jimoh@yahoo.com>
2021-04-26 15:58:53 +0100

    initial commits

--- genomics/main.py
+++ genomics/main.py
@@ -30,13 +30,25 @@ class Sequencing:
         else:
             return False
 
+    def frequency(self):
+        """ Calculates the frequency of each symbol in the sequence.
+            Returns a dictionary. """
+        dic = {}
+        for s in self.sequence.upper():
+            if s in dic:
+                dic[s] += 1
+            else : dic[s] = 1
+        return dic
 
-
-    # def frequency(self, dna_sequence):
-
+    def gc_count(self):
+        "calculate the number of GC content in sequence"
+        gc_ct = self.sequence.upper().count("G") + self.sequence.upper().count("C")
+        gc_percent = (gc_ct/len(self.sequence))*100
+        return f"gc count is {gc_ct} and percent gc content is {gc_percent}"
 
 if __name__ == '__main__':
-    obj = Sequencing("ACTGGUGGGGxdfss")
+    obj = Sequencing("ACCCCCCCTGGUGGGGxdfss")
     print(obj.validate_dna())
     print(obj.validate_rna())
-
+    print(obj.frequency())
+    print(obj.gc_count())
